81a5f2376eb986c5742db5ac75924413470685d1
rua1hc <tatut90@gmail.com>
2022-06-30 00:25:40 +0700

    PG base method modify child value

---
+++ playground.js
@@ -0,0 +1,27 @@
+class Base {
+    baseValue = 1;
+    baseAdd() {
+        this.childValue++;
+        console.log("baseAdd:", this.childValue);
+    }
+}
+
+class Child extends Base {
+    childValue = 10;
+    childAdd() {
+        this.baseValue++;
+        console.log("childAdd:", this.baseValue);
+    }
+}
+
+let b = new Base();
+let c = new Child();
+
+console.log("=====m=====");
+b.baseAdd();
+c.baseAdd();
+
+console.log("=====m=====");
+
+// b.childAdd(); // ERROR!
+c.childAdd();
